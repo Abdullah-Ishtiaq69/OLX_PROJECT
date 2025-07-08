f9e8695dda1fa23133846834afc59f307851aa26
abdullah ishtiaq <abdullahishtiaq335@gmail.com>
2025-07-08 14:01:45 -0700

    Login & SignUp Fully Compelete

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
+// page navigation
+
 let motorPage = document.querySelectorAll(".motor");
 let propertyPage = document.querySelectorAll(".property");
 let homePage = document.querySelectorAll(".logo");
 
-// Page Navigation function
-const navigationPage = (page , url) => {
+const navigationPage = (page, url) => {
     page.forEach((element) => {
-        element.addEventListener("click" , () => {
+        element.addEventListener("click", () => {
             window.location.href = url
         });
     });
 };
 
-// Page Navigation function
-navigationPage(homePage , "index.html");
-navigationPage(motorPage , "motor.html");
-navigationPage(propertyPage , "property.html");
+navigationPage(homePage, "index.html");
+navigationPage(motorPage, "motor.html");
+navigationPage(propertyPage, "property.html");
+
+// login or signup text change
+
+let loginHeading = document.getElementById("login-heading");
+let loginText = document.getElementById("login-text");
+
+let isChanged = false;
+
+const changeText = () => {
+    loginHeading.innerText = "Create a new OLX account";
+    loginText.innerText = "Already have an account? Log in"
+    isChanged = true
+}
+
+const resetText = () => {
+    loginHeading.innerText = "Login into your OLX account";
+    loginText.innerText = "New to OLX? Create an account";
+    isChanged = false;
+}
+
+loginText.addEventListener("click", () => {
+    if (!isChanged) {
+        changeText();
+    } else {
+        resetText();
+    }
+})
+
+document.body.addEventListener("click", (e) => {
+    if (e.target.classList.contains("modal") || e.target.classList.contains("btn-close")) {
+        resetText();
+    }
+})
